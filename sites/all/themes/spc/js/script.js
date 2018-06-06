c18b53f40c19435cdba658b19fe2573dc035cbdf
lrudenko <lilia.rudenko@linkdigital.com.au>
2018-06-06 17:34:26 +1000

    SPCXCFZB-46 / Thematic Landing Page Articles updated

--- sites/all/themes/spc/js/script.js
+++ sites/all/themes/spc/js/script.js
@@ -1,5 +1,12 @@
-/*
 (function ($) {
- //...
+    var titles = $('#mini-panel-thematic_articles_and_publicatio .thematic-group-articles-item h4 a')
+    if (titles) {
+        titles.each(function(){
+            var title = this.innerText;
+            if (title.length > 40) {
+                var shortTitle = $.trim(title).substring(0, 40).split(' ').slice(0, -1).join(' ') + '...';
+                this.innerText = shortTitle;
+            }
+        })
+    }
 })(jQuery);
-*/
