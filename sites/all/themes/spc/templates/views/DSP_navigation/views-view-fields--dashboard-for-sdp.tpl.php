a34295c2149f4402e28dd06f3332b69d34f16627
lrudenko <lilia.rudenko@linkdigital.com.au>
2018-09-05 22:54:48 +1000

    SPCXCFZB-121 / Updated DSP logo implementation

--- sites/all/themes/spc/templates/views/DSP_navigation/views-view-fields--dashboard-for-sdp.tpl.php
+++ sites/all/themes/spc/templates/views/DSP_navigation/views-view-fields--dashboard-for-sdp.tpl.php
@@ -5,12 +5,21 @@
     break;
   };
 
-  foreach ($indicators as $id => $ind) {
-    $nid = $ind['target_id'];
-    $node = node_load($nid);
-    $path = 'node/' . (int)$nid;
-    $alias = drupal_get_path_alias($path); 
-    print('<li><a href="/'.$alias.'">'.$node->title.'</a></li>');
-    
-  };
+  $icon_uri = $node->field_icon_logo ? $node->field_icon_logo['und'][0]['uri'] : '';
 ?>
+<button type="button" id="goals-navigation" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  <img class="goals-logo" src="<?php !empty($icon_uri) ? print(file_create_url($icon_uri)) : '/sites/all/themes/spc/img/spc/SDG_logo.png'; ?>">
+    <i class="fa fa-caret-down" aria-hidden="true"></i>
+    <i class="fa fa-caret-up" aria-hidden="true"></i>
+</button>
+<ul class="dropdown-menu" aria-labelledby="goals-navigation">
+  <?php
+    foreach ($indicators as $id => $ind) {
+      $nid = $ind['target_id'];
+      $node = node_load($nid);
+      $path = 'node/' . (int)$nid;
+      $alias = drupal_get_path_alias($path); 
+      print('<li><a href="/'.$alias.'">'.$node->title.'</a></li>');
+    };
+    ?>
+</ul>
