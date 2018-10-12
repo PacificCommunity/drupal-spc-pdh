182b41a6594593e4ba11d5176ebc048d0c1a092d
Aleksey Dorozhkin <aleksey.dorozhkin@linkdigital.com.au>
2018-10-12 17:52:54 +0300

    SPCXCFZB-170 / New templates were added

---
+++ sites/all/themes/spc/templates/panes/panels-pane--views-panes--articles_list_home_latest.tpl.php
@@ -0,0 +1,13 @@
+<div class="<?php print $classes; ?>" <?php  print $id; ?>>
+	<div class="container">
+		<?php if ($admin_links): ?>
+			<?php print $admin_links; ?>
+		<?php endif; ?>
+		<?php if ($title): ?>
+			<h2 class="pane-title"><?php print $title; ?></h2>
+		<?php endif; ?>
+		<div>
+			<?php print render($content); ?>
+		</div>
+	</div>
+</div>
