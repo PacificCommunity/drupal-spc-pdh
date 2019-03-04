f5e4eea095c2fc67fbd0881228b4ee3241340683
lrudenko <lilia.rudenko@linkdigital.com.au>
2019-03-04 21:14:17 +1100

    SPCXCFZB-295 / Stories carousel styled

--- sites/all/themes/spc/templates/panes/panels-pane--views-panes--articles_list_home_latest.tpl.php
+++ sites/all/themes/spc/templates/panes/panels-pane--views-panes--articles_list_home_latest.tpl.php
@@ -1,13 +1,11 @@
 <div class="<?php print $classes; ?>" <?php  print $id; ?>>
-	<div class="container-fluid">
-		<?php if ($admin_links): ?>
-			<?php print $admin_links; ?>
-		<?php endif; ?>
-		<?php if ($title): ?>
-			<h2 class="pane-title"><?php print $title; ?></h2>
-		<?php endif; ?>
-		<div>
-			<?php print render($content); ?>
-		</div>
+	<?php if ($admin_links): ?>
+		<?php print $admin_links; ?>
+	<?php endif; ?>
+	<?php if ($title): ?>
+		<h2 class="pane-title"><?php print $title; ?></h2>
+	<?php endif; ?>
+	<div>
+		<?php print render($content); ?>
 	</div>
 </div>
