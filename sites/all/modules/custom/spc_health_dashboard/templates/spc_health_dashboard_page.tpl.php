06f8332d7659b2df3def4456b351bb945270feeb
Alex Pavlyuk <alexpavlyuk@MacBook-Pro-Alex.local>
2019-07-05 15:40:17 +0300

    SPCXCFZB-390 / Countries, Categories, Indicators, JSON example, Basic dashboard  page styles and templates.

---
+++ sites/all/modules/custom/spc_health_dashboard/templates/spc_health_dashboard_page.tpl.php
@@ -0,0 +1,19 @@
+<div class="bean-banner-with-title">
+    <div class="content">
+        <div style="" class="banner-with-title health-dashboard-header">
+            <div class="left-label"><strong>NCD</strong> Dashboard</div>
+            <div class="breadcrumbs"><?php print $breadcrumbs; ?></div>
+            <div class="field field-type-text-long">
+                <h1><?php print $title; ?> </h1>
+            </div>
+            <div class="search"><?php print $search; ?></div>
+        </div>
+    </div>
+</div>
+<div role="main" id="main-content" class="health-dashboard-content">
+    <div class="container">
+      <?php print $subtitle; ?>
+      <br>
+      <?php print $content; ?>
+    </div>
+</div>
