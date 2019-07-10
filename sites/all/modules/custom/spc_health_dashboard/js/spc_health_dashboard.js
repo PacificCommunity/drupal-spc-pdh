c35492963b1d6b24ac69199d4563938ebf766964
shchedrin <shchedrin@qarea.com>
2019-07-10 19:21:50 +0300

    added country related categories and toggle

--- sites/all/modules/custom/spc_health_dashboard/js/spc_health_dashboard.js
+++ sites/all/modules/custom/spc_health_dashboard/js/spc_health_dashboard.js
@@ -34,134 +34,145 @@
                 .style("opacity", 0);
         }
         
-        const width = 900,
-            height = 400;
+        if ($('.stacked-chart-global').length){
+            const width = 900,
+                height = 400;
 
-        // Set x, y and colors
-        let x = d3.scale.ordinal()
-          .domain(dataset[0].map(function(d) { return d.x; }))
-          .rangeRoundBands([10, width-10], 0.02);
+            // Set x, y and colors
+            let x = d3.scale.ordinal()
+              .domain(dataset[0].map(function(d) { return d.x; }))
+              .rangeRoundBands([10, width-10], 0.02);
 
-        let y = d3.scale.linear()
-          .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
-          .range([height, 0]);
+            let y = d3.scale.linear()
+              .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
+              .range([height, 0]);
 
-        const colors = ["#cfff6fb3", "#ffc00080", "#ff000080" ];
-        const border = ['#CFFF6F',  '#FFC000', '#FF0000' ];
-        const range = ["Present", "Under Development", "Not Present"];
-        const rangeWidth = [95, 170, 120];
+            const colors = ["#cfff6fb3", "#ffc00080", "#ff000080" ];
+            const border = ['#CFFF6F',  '#FFC000', '#FF0000' ];
+            const range = ["Present", "Under Development", "Not Present"];
+            const rangeWidth = [95, 170, 120];
+
+            let svg = d3.select(".stacked-chart-global")
+              .append("svg")
+              .attr("viewBox", [0, 0, width, height])
+              .append("g"); 
+
+            let defs = svg.append("defs");
+
+            let filter = defs.append("filter")
+                .attr("id", "dropshadow")
+
+            filter.append("feGaussianBlur")
+                .attr("in", "SourceAlpha")
+                .attr("stdDeviation", 4)
+                .attr("result", "blur");
+
+            filter.append("feOffset")
+                .attr("in", "blur")
+                .attr("dx", 2)
+                .attr("dy", 2)
+                .attr("result", "offsetBlur");
+
+            let feMerge = filter.append("feMerge");
+
+            feMerge.append("feMergeNode")
+                .attr("in", "offsetBlur")
+            feMerge.append("feMergeNode")
+                .attr("in", "SourceGraphic");  
+
+            let tooltext = setToolText(svg, "tooltip");
+            let tooltip = setToolBox(svg);
+
+            document
+                .querySelector(".stacked-chart-global svg")
+                .appendChild(document.querySelector(".tipbox"));
+
+            document
+                .querySelector(".stacked-chart-global svg")
+                .appendChild(document.querySelector(".tooltip"));     
+
+            // Define and draw axes
+            let yAxis = d3.svg.axis()
+              .scale(y)
+              .orient("left")
+              .ticks(5)
+              .tickSize(-width, 0, 0)
+              .tickFormat( function(d) { return d } );
+
+            let xAxis = d3.svg.axis()
+              .scale(x)
+              .orient("bottom");
+
+            svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis);
+
+            svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .style("font-size", '11')
+              .call(xAxis);
+
+            svg.selectAll(".x .domain").style("opacity", 0);
+
+            // Create groups for each series, rects for each segment 
+            let groups = svg.selectAll("g.cost")
+                .data(dataset)
+                .enter()
+                .append("g")
+                .attr("class", "cost")
+                .attr('range', function(d, i) { return range[i]; })
+                .attr('range-width', function(d, i) { return rangeWidth[i]; })
+                .attr('stroke', function(d, i) { return border[i]; })
+                .attr('def-fill', function(d, i) { return colors[i]; })
+                .style("stroke-width", 1)
+                .style("fill", function(d, i) { return colors[i]; });
+
+            groups.selectAll("rect")
+                .data(function(d) { return d; })
+                .enter()
+                .append("rect")
+                .attr("rx", 5)
+                .attr("ry", 5)
+                .attr('value', function(d) { return d.y; })
+                .attr("x", function(d) { return x(d.x)+40; })
+                .attr("y", function(d) { return y(d.y0 + d.y); })
+                .attr("height", function(d) { return (y(d.y0) - y(d.y0 + d.y))-5 ; })
+                .attr("width", 30)
+                .on('mouseover', function(d){
+                    let hovRange = d3.select(this.parentNode).attr('range');
+                    let hovRangeWidth = d3.select(this.parentNode).attr('range-width');
+                    let hovFill = d3.select(this.parentNode).attr('stroke');
+                    d3.select(this).attr('fill', hovFill);
+
+                    tooltip.style("opacity", 1)
+                        .attr("x", x(d.x)+75)
+                        .attr("y", y(d.y0 + d.y))
+                        .attr("width", hovRangeWidth);
+
+                    tooltext.style("opacity", 1)      
+                        .text(hovRange + ' ' + Math.round(d.y) + '%')  
+                        .attr("x", x(d.x)+85)
+                        .attr("y", y(d.y0 + d.y)+23);
+
+                })
+                .on('mouseout', function(d){
+                    let defFill = d3.select(this.parentNode).attr('def-fill');
+                    d3.select(this).attr('fill', defFill);
+
+                    tooltip.style("opacity", 0)
+                    tooltext.style("opacity", 0)
+                });            
+        }
         
-        let svg = d3.select(".stacked-chart-global")
-          .append("svg")
-          .attr("viewBox", [0, 0, width, height])
-          .append("g"); 
-  
-        let defs = svg.append("defs");
-
-        let filter = defs.append("filter")
-            .attr("id", "dropshadow")
-    
-        filter.append("feGaussianBlur")
-            .attr("in", "SourceAlpha")
-            .attr("stdDeviation", 4)
-            .attr("result", "blur");
-    
-        filter.append("feOffset")
-            .attr("in", "blur")
-            .attr("dx", 2)
-            .attr("dy", 2)
-            .attr("result", "offsetBlur");
-    
-        let feMerge = filter.append("feMerge");
-
-        feMerge.append("feMergeNode")
-            .attr("in", "offsetBlur")
-        feMerge.append("feMergeNode")
-            .attr("in", "SourceGraphic");  
-  
-        let tooltext = setToolText(svg, "tooltip");
-        let tooltip = setToolBox(svg);
-
-        document
-            .querySelector(".stacked-chart-global svg")
-            .appendChild(document.querySelector(".tipbox"));
-    
-        document
-            .querySelector(".stacked-chart-global svg")
-            .appendChild(document.querySelector(".tooltip"));     
-
-        // Define and draw axes
-        let yAxis = d3.svg.axis()
-          .scale(y)
-          .orient("left")
-          .ticks(5)
-          .tickSize(-width, 0, 0)
-          .tickFormat( function(d) { return d } );
-
-        let xAxis = d3.svg.axis()
-          .scale(x)
-          .orient("bottom");
-
-        svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis);
-
-        svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .style("font-size", '11')
-          .call(xAxis);
-  
-        svg.selectAll(".x .domain").style("opacity", 0);
-
-        // Create groups for each series, rects for each segment 
-        let groups = svg.selectAll("g.cost")
-            .data(dataset)
-            .enter()
-            .append("g")
-            .attr("class", "cost")
-            .attr('range', function(d, i) { return range[i]; })
-            .attr('range-width', function(d, i) { return rangeWidth[i]; })
-            .attr('stroke', function(d, i) { return border[i]; })
-            .attr('def-fill', function(d, i) { return colors[i]; })
-            .style("stroke-width", 1)
-            .style("fill", function(d, i) { return colors[i]; });
-
-        groups.selectAll("rect")
-            .data(function(d) { return d; })
-            .enter()
-            .append("rect")
-            .attr("rx", 5)
-            .attr("ry", 5)
-            .attr('value', function(d) { return d.y; })
-            .attr("x", function(d) { return x(d.x)+40; })
-            .attr("y", function(d) { return y(d.y0 + d.y); })
-            .attr("height", function(d) { return (y(d.y0) - y(d.y0 + d.y))-5 ; })
-            .attr("width", 30)
-            .on('mouseover', function(d){
-                let hovRange = d3.select(this.parentNode).attr('range');
-                let hovRangeWidth = d3.select(this.parentNode).attr('range-width');
-                let hovFill = d3.select(this.parentNode).attr('stroke');
-                d3.select(this).attr('fill', hovFill);
-
-                tooltip.style("opacity", 1)
-                    .attr("x", x(d.x)+75)
-                    .attr("y", y(d.y0 + d.y))
-                    .attr("width", hovRangeWidth);
-             
-                tooltext.style("opacity", 1)      
-                    .text(hovRange + ' ' + Math.round(d.y) + '%')  
-                    .attr("x", x(d.x)+85)
-                    .attr("y", y(d.y0 + d.y)+23);
-
-            })
-            .on('mouseout', function(d){
-                let defFill = d3.select(this.parentNode).attr('def-fill');
-                d3.select(this).attr('fill', defFill);
-                
-                tooltip.style("opacity", 0)
-                tooltext.style("opacity", 0)
-            });
+        $('.toggle').on('click', function(e){
+            e.preventDefault();
+            
+            $(this).siblings('.helth-indicators').toggle('slow'); 
+            
+        });
+            
+            
        
         //end context
         }
