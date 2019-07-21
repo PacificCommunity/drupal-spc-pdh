f5bb9257ec4f579b1967ce99a34eaa35807fce74
shchedrin <shchedrin@qarea.com>
2019-07-21 12:22:03 +0300

    added ed dashboard link on home page

---
+++ sites/all/modules/custom/spc_education_dashboard/templates/block--education_dashboard_home_link.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file
+ *
+ * Theme to display the Education dashboard block on a Drupal page.
+ *
+ */
+?>
+
+<div class="ed-dashboard-link-container">
+    <div about="/content/17-goals-transform-pacific" typeof="sioc:Item foaf:Document" class="ds-1col node node-dashboards node-promoted contextual-links-region view-mode-dashboard_highlight clearfix">
+
+        <div class="contextual-links-wrapper contextual-links-processed">
+            <a class="contextual-links-trigger" href="#">Configure</a>
+        </div>  
+
+        <div class="group-content">
+            <div class="group-content-left">
+                The <strong> status of education </strong> in the Pacific region
+            </div>
+            <div class="group-content-right">
+                <div class="dashboard-description">
+                    This Dashboard will enable monitoring of progress against the 2030 agenda for sustainable development
+                </div>
+                <div class="field field-type-ds">
+                    <a href="/education-dashboard" class="banner-button btn">View DASHBOARD</a>        
+                </div>
+            </div>
+
+        </div>
+        <div class="group-chart">
+            <div class="chart-static" style="background: url(/sites/all/modules/custom/spc_education_dashboard/img/education-home.svg) 50% 50% no-repeat"></div>
+        </div>
+        
+    </div>        
+</div>
