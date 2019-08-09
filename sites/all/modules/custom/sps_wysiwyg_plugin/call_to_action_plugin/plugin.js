716b41cbd64f25a4858f37140e6ed841e7a69b4c
shchedrin <shchedrin@qarea.com>
2019-08-09 10:38:14 +0300

    added CTA callback

---
+++ sites/all/modules/custom/sps_wysiwyg_plugin/call_to_action_plugin/plugin.js
@@ -0,0 +1,28 @@
+(function($) {
+    CKEDITOR.plugins.add( 'call_to_action_plugin', {
+        init: function( editor ){
+
+            const styles = ''
+                +'display: inline-block;'
+                + 'background: #001E73;'
+                + 'padding: 6px 22px;'
+                + 'border-radius: 20px;'
+                + 'color: #fff;'
+                + 'margin: 10px 0px;';
+                
+            const text = 'Call To Action';
+
+            editor.addCommand( 'call_to_action_command', {
+                exec : function( editor ) {
+                    editor.insertHtml( '<a href="" style="' + styles + '">' + text + '</a>' );
+                }
+            });
+
+            editor.ui.addButton( 'call_to_action_button', {
+                label: 'Call to Action Button',
+                command: 'call_to_action_command',
+                icon: this.path + 'images/action_button_fill.svg'
+            });
+        }
+    });
+})(jQuery);
