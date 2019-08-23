f8c461b788385d75c618bd234fafafc40ab18cf7
shchedrin <shchedrin@qarea.com>
2019-08-23 13:01:50 +0300

    remove chenges not related to SXSPCXZFI-16 tiket

--- sites/all/modules/custom/spc_education_dashboard/js/education_dashboard.js
+++ sites/all/modules/custom/spc_education_dashboard/js/education_dashboard.js
@@ -369,28 +369,14 @@
             }
             
             function setChartSvg(chart, width, height, className){
-                let svg = {};
-                const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-                if (isIE11){
-                    svg = d3.select(chart)
-                        .append("svg")
-                        .attr('version', 1.1)
-                        .attr('xmlns', 'http://www.w3.org/2000/svg')
-                        .attr('class', className)
-                        .attr("height", height)
-                        .attr("viewBox", [0, 0, width, height])
-                        .append("g");
-                } else {
-                    svg = d3.select(chart)
-                        .append("svg")
-                        .attr('version', 1.1)
-                        .attr('xmlns', 'http://www.w3.org/2000/svg')
-                        .attr('class', className)
-                        .attr("viewBox", [0, 0, width, height])
-                        .append("g");
-                }
-            
-                return svg;
+                return d3
+                    .select(chart)
+                    .append("svg")
+                    .attr('version', 1.1)
+                    .attr('xmlns', 'http://www.w3.org/2000/svg')
+                    .attr('class', className)
+                    .attr("viewBox", [0, 0, width, height])
+                    .append("g");
             }
             
             function setSvgDomains(data, x, y){
@@ -544,31 +530,14 @@
             }
             
             function setGenderChartSvg(id, width, height, margin){
-                let svg = {};
-                const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-                
-                if (isIE11){
-                    svg = d3
-                        .select('.chart-' + id)
-                        .append("svg")
-                        .attr('version', 1.1)
-                        .attr('xmlns', 'http://www.w3.org/2000/svg')
-                        .attr("height", height + margin.top + margin.bottom)
-                        .attr("viewBox", [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom])
-                        .append("g")
-                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-                } else {
-                    svg = d3
-                        .select('.chart-' + id)
-                        .append("svg")
-                        .attr('version', 1.1)
-                        .attr('xmlns', 'http://www.w3.org/2000/svg')
-                        .attr("viewBox", [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom])
-                        .append("g")
-                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");                     
-                }
-
-                return svg;
+                return d3
+                    .select('.chart-' + id)
+                    .append("svg")
+                    .attr('version', 1.1)
+                    .attr('xmlns', 'http://www.w3.org/2000/svg')
+                    .attr("viewBox", [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom])
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
             }
             
             function setSvgGenderBarData(svg, data, x0, x1, y, width, height, tooltip, tooltext, boyWrap){
